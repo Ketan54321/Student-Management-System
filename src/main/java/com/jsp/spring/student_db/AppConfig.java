8ac40b4d816281f252bc1a793e15b8f7bba83922
Ketan54321 <ketanpavadshetty@email>
2025-04-02 16:57:59 +0530

    adding web initiolizer and app config class

---
+++ src/main/java/com/jsp/spring/student_db/AppConfig.java
@@ -0,0 +1,10 @@
+package com.jsp.spring.student_db;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.jsp.spring")
+public class AppConfig {
+
+}
